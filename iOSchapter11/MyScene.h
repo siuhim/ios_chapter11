9dee95c51fa6a4731209fb90083cca9c152b5585
siuhim <siuhim.tang@gmail.com>
2014-03-31 22:57:59 -0400

    Initial Commit

---
+++ iOSchapter11/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  iOSchapter11
+//
+
+//  Copyright (c) 2014 Jimmy Tang. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ iOSchapter11Tests/iOSchapter11Tests.m
@@ -0,0 +1,34 @@
+//
+//  iOSchapter11Tests.m
+//  iOSchapter11Tests
+//
+//  Created by Jimmy Tang on 3/31/14.
+//  Copyright (c) 2014 Jimmy Tang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSchapter11Tests : XCTestCase
+
+@end
+
+@implementation iOSchapter11Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
